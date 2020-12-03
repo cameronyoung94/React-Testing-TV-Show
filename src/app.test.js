292d560444ee167a4b6dc6516ee7aa24636c8ada
cameronyoung94 <youngcameron0794@gmail.com>
2020-12-02 23:15:11 -0500

    started tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { fireEvent, render, wait } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import App from './App';
-import { fetchShow as mockFetchMissions } from './api/fetchShow';
+import { fetchShow as mockFetchShow } from './api/fetchShow';
+import {episodesFixture} from './components/episodes.test'
+import userEvent from '@testing-library/user-event';
 
-jest.mock('./api/fetchShow');
+  
+test('App fetches and renders data', async()=>{
 
-test("App renders", () => {
-  render(<App />);
-});
+    const {getByText,getAllByText}=render(<App/>);
+
+    await waitFor(()=>{getByText(/select a season/i)}) ;
+
+    const button=getByText(/select a season/i)
+    userEvent.click(button);
+
+    const option= getByText(/Season 2/i);
+    userEvent.click(option);
+       expect(getAllByText(/episode/i)).toHaveLength(9)
+
+    }); 
 
 
+    
 
 
